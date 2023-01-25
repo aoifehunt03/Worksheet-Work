dd84dee24ca275b9d427c1a4a4c38dfdc66e2a88
S00236573 <S00236573@LAB-D2039-23>
2023-01-25 10:20:45 +0000

    Add project files.

---
+++ Q1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintIntegersFor();
+        }
+
+        static int PrintIntegersFor()
+        {
+            int i = 40;
+            while (i <= 60)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            return i;
+        }
+    }
+}
